2358ed2f34ee37f21ff22b748a29acf451ec5d1e
yumueat <1471063903@qq.com>
2023-03-15 20:04:56 +0800

    更新1.0.5

--- WebsiteDirectoryScanner/WebsiteDirectoryScanner.py
+++ WebsiteDirectoryScanner/WebsiteDirectoryScanner.py
@@ -1,26 +1,30 @@
 # -*- coding: utf-8 -*-
 """
 @author yumu
-@version 1.0.4
+@version 1.0.5
 """
 import argparse
-__version__ = "1.0.4"
+
+__version__ = "1.0.5"
 __mode2directory__ = {
-    '1' : "./directory/dir_dic.txt"
+    '1': "./directory/dir_dic.txt"
 }
-import os,sys
+
+import os, sys
 import requests
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils import *
 
-
 colorPrinter = ColorPrinter()
-def scan(url,url_list,directory_list,mode,outputname,outputpath,quite):
+
+
+def scan(url, url_list, directory_list, mode, outputname, outputpath, quite, timeout):
     target_url = []
     if url_list != None:
         for url_file in url_list:
             try:
-                with open(url_file,"r") as f:
+                with open(url_file, "r") as f:
                     urls = f.readlines()
                 for single_url in urls:
                     single_url = single_url.strip()
@@ -34,12 +38,12 @@ def scan(url,url_list,directory_list,mode,outputname,outputpath,quite):
     if directory_list != None:
         try:
             for directory_file in directory_list:
-                with open(directory_file,"r") as f:
+                with open(directory_file, "r") as f:
                     directorys = f.readlines()
                 for single_directory in directorys:
                     single_directory = single_directory.strip()
                     if single_directory[0] != "/":
-                        single_directory = "/"+single_directory
+                        single_directory = "/" + single_directory
                     target_directory.append(single_directory)
         except:
             print(colorPrinter.wrong_text("[-] 指定的字典文件读取失败，请检查路径和文件名以及是否有读取权限"))
@@ -54,7 +58,7 @@ def scan(url,url_list,directory_list,mode,outputname,outputpath,quite):
     if mode != None:
         for m in mode:
             try:
-                with open(__mode2directory__[m],"r") as f:
+                with open(__mode2directory__[m], "r") as f:
                     directorys = f.readlines()
                 for single_directory in directorys:
                     single_directory = single_directory.strip()
@@ -66,15 +70,19 @@ def scan(url,url_list,directory_list,mode,outputname,outputpath,quite):
 
     if len(target_url) == 0:
         print(colorPrinter.wrong_text("没有要扫描的url"))
+        return
     if len(target_directory) == 0:
         print(colorPrinter.wrong_text("没有指定的字典"))
+        return
     log_info = []
+    if timeout == None:
+        timeout = 5
     for u in target_url:
-        print(colorPrinter.special_text("正在扫描"+u))
-        log_info.append(u+"的扫描结果如下")
+        print(colorPrinter.special_text("正在扫描" + u))
+        log_info.append(u + "的扫描结果如下")
         for dir in target_directory:
             try:
-                resp = requests.get(u+dir)
+                resp = requests.get(u + dir, timeout=int(timeout))
             except:
                 continue
             if quite:
@@ -83,10 +91,10 @@ def scan(url,url_list,directory_list,mode,outputname,outputpath,quite):
                     log_info.append(u + dir + "   " + str(resp.status_code))
             else:
                 if resp.status_code == 200:
-                    print(colorPrinter.info_text(u+dir + "   " + str(resp.status_code)))
-                    log_info.append(u+dir + "   " + str(resp.status_code))
+                    print(colorPrinter.info_text(u + dir + "   " + str(resp.status_code)))
+                    log_info.append(u + dir + "   " + str(resp.status_code))
                 else:
-                    print(colorPrinter.warn_text(u+dir + "   " + str(resp.status_code)))
+                    print(colorPrinter.warn_text(u + dir + "   " + str(resp.status_code)))
 
     index = 1
     default_path = "../result/WebsiteDirectoryScanner/"
@@ -98,12 +106,12 @@ def scan(url,url_list,directory_list,mode,outputname,outputpath,quite):
     if not os.path.exists(default_path):
         os.makedirs(default_path)
 
-    if outputname==None:
-        while(os.path.exists(default_path+str(index)+".txt")):
-            index +=1
-        full_path = default_path+str(index)+".txt"
+    if outputname == None:
+        while (os.path.exists(default_path + str(index) + ".txt")):
+            index += 1
+        full_path = default_path + str(index) + ".txt"
     else:
-        full_path = default_path+outputname[0]
+        full_path = default_path + outputname[0]
     try:
         with open(full_path, "w") as f:
             for log in log_info:
@@ -111,6 +119,7 @@ def scan(url,url_list,directory_list,mode,outputname,outputpath,quite):
         print("[+] 文件已经保存到 " + colorPrinter.wrong_text(os.path.abspath(full_path)))
     except:
         print(colorPrinter.wrong_text("[-] 文件写入失败，请检查路径和文件名以及是否有写入权限"))
+
 
 def get_parser():
     """
@@ -178,9 +187,14 @@ def get_parser():
         action="store_true",
         help="指定为安静模式，只输出200的url"
     )
-
-
+    group.add_argument(
+        "-to",
+        "--timeout",
+        action="store",
+        help="指定超时时间"
+    )
     return parser
+
 
 def show_version():
     """
@@ -189,9 +203,11 @@ def show_version():
     """
     print(colorPrinter.wrong_text("[ 网站目录扫描器 ]  " + __version__))
 
+
 def print_tool_info():
     print("[ yumueat | https://github.com/yumueat]")
     print("[ Website Directory Scanner | https://github.com/yumueat/Penetration-test-toolkit]")
+
 
 def main():
     parser = get_parser()
@@ -200,7 +216,7 @@ def main():
     if args.version:
         show_version()
     elif args.scan:
-        scan(args.url,args.list,args.directory,args.mode,args.outputname,args.outputpath,args.quite)
+        scan(args.url, args.list, args.directory, args.mode, args.outputname, args.outputpath, args.quite, args.timeout)
     else:
         parser.print_help()
 
